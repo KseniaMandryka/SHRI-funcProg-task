d093d07e53cc99c4ff9e0dadd8ba4ed902fc079e
Ksenia Mandryka <ksenia_mandryka@mail.ru>
2023-07-23 18:01:51 +0300

    refactor processSequence

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,89 @@
-/**
- * @file Домашка по FP ч. 2
- *
- * Подсказки:
- * Метод get у инстанса Api – каррированый
- * GET / https://animals.tech/{id}
- *
- * GET / https://api.tech/numbers/base
- * params:
- * – number [Int] – число
- * – from [Int] – из какой системы счисления
- * – to [Int] – в какую систему счисления
- *
- * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
- * Ответ будет приходить в поле {result}
- */
- import Api from '../tools/api';
+import Api from '../tools/api'
+import { __, andThen, assoc, concat, gt, ifElse, length, lt, mathMod, otherwise, partial, tap, test } from "ramda"
 
- const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+function allPass(fns) {
+  return arg => fns.every(fn => fn(arg))
+}
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const compose = (...fns) => {
+  return arg => {
+    return fns.reduceRight((acc, fn) => fn(acc), arg)
+  }
+}
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const pipe = (...fns) => {
+  return arg => {
+    return fns.reduce((acc, fn) => fn(acc), arg)
+  }
+}
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const prop = key => obj => obj[key]
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+const api = new Api()
 
-export default processSequence;
+const square = num => num ** 2
+const gtTwo = gt(__, 2)
+const ltTen = lt(__, 10)
+
+const thenSquare = andThen(square)
+
+const lengthGreaterThenTwo = compose(gtTwo, length)
+const lengthLowerThenTen = compose(ltTen, length)
+const testOnlyNumbers = test(/^[0-9]+\.?[0-9]+$/)
+const roundStringToNumber = compose(Math.round, Number)
+const modForThreeToString = compose(String, mathMod(__, 3))
+
+const thenModOfThreeToString = andThen(modForThreeToString)
+const thenGetLength = andThen(length)
+
+const validate = allPass([lengthGreaterThenTwo, lengthLowerThenTen, testOnlyNumbers])
+
+const assocNumberToBinary = assoc('number', __, { from: 10, to: 2 })
+
+const API_NUMBERS_URL = 'https://api.tech/numbers/base'
+const API_ANIMALS_URL = 'https://animals.tech/'
+const getApiResult = compose(String, prop('result'))
+const thenGetApiResult = andThen(getApiResult)
+const thenConcatToAnimalsUrl = andThen(concat(API_ANIMALS_URL))
+const thenCallApiWithEmptyParams = andThen(api.get(__, {}))
+const apiGetNumberBinaryBase = compose(
+  api.get(API_NUMBERS_URL),
+  assocNumberToBinary
+)
+
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+  const tapLog = tap(writeLog)
+  const thenTapLog = andThen(tapLog)
+  const thenHandleSuccess = andThen(handleSuccess)
+  const otherwiseHandleError = otherwise(handleError)
+
+  const handleValidationError = partial(handleError, ['ValidationError'])
+
+  const sequenceComposition = pipe(
+    roundStringToNumber,
+    tapLog,
+    apiGetNumberBinaryBase,
+    thenGetApiResult,
+    thenTapLog,
+    thenGetLength,
+    thenTapLog,
+    thenSquare,
+    thenTapLog,
+    thenModOfThreeToString,
+    thenTapLog,
+    thenConcatToAnimalsUrl,
+    thenCallApiWithEmptyParams,
+    thenGetApiResult,
+    thenHandleSuccess,
+    otherwiseHandleError,
+  )
+
+  const runWithCondition = ifElse(validate, sequenceComposition, handleValidationError)
+  const logAndRunSequence = compose(runWithCondition, tapLog)
+
+  logAndRunSequence(value)
+}
+
+export default processSequence
